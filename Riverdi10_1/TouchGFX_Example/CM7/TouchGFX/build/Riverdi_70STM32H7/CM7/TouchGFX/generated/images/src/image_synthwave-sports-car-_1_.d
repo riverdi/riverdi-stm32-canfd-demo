4e6910dab28f7e196acfa5e8c31695231aa1e085
Hussam Aldean <31468020+hussamaldean@users.noreply.github.com>
2024-03-06 08:52:40 +0300

    Added TouchGFX example for 10.1 inch
    A working TouchGFX example has been added to ensure future projects has starting point.

---
+++ Riverdi10_1/TouchGFX_Example/CM7/TouchGFX/build/Riverdi_70STM32H7/CM7/TouchGFX/generated/images/src/image_synthwave-sports-car-_1_.d
@@ -0,0 +1,5 @@
+CM7/TouchGFX/build/Riverdi_70STM32H7/CM7/TouchGFX/generated/images/src/image_synthwave-sports-car-_1_.o: \
+ CM7/TouchGFX/generated/images/src/image_synthwave-sports-car-_1_.cpp \
+ CM7/Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+CM7/Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
